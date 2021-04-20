7508f9f02077c013789da5d0c02f765cf684b348
rmill <rmiller003@gmail.com>
2021-04-20 14:02:40 -0400

    Stage 2

--- main.py
+++ main.py
@@ -25,3 +25,43 @@ analysis = TextBlob(article.text)
 print(analysis.polarity)
 print(f'Sentiment: {"positive" if analysis.polarity > 0 else "negative" if analysis.polarity < 0 else "neutral"}')
 
+root = tk.Tk()
+root.title("TechNews_Summarizer-X")
+root.geometry('1200x600')
+
+tlabel = tk.Label(root, text="Title)")
+tlabel.pack()
+
+title = tk.Text(root, height=1, width=140)
+title.config(state='disabled', bg='#dddddd')
+title.pack()
+
+alabel = tk.Label(root, text="Author")
+alabel.pack()
+
+author = tk.Text(root, height=1, width=140)
+author.config(state='disabled', bg='#dddddd')
+author.pack()
+
+plabel = tk.Label(root, text="Publishing Date")
+plabel.pack()
+
+publication = tk.Text(root, height=1, width=140)
+publication.config(state='disabled', bg='#dddddd')
+publication.pack()
+
+slabel = tk.Label(root, text="News Summary")
+slabel.pack()
+
+summary = tk.Text(root, height=20, width=140)
+summary.config(state='disabled', bg='#dddddd')
+summary.pack()
+
+slabel = tk.Label(root, text="Sentiment Analysis")
+slabel.pack()
+
+sentiment = tk.Text(root, height=1, width=140)
+sentiment.config(state='disabled', bg='#dddddd')
+sentiment.pack()
+
+root.mainloop()
